cc7f2c395db4bcc4e2238a1604bdcff0be3cf1fe
Alvaro Puente <alvaropuentecorrea@gmail.com>
2023-08-27 05:04:41 +0200

    add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import './styles.css'
 import { ShowItems } from './todo/pages/ItemsPage'
 import { Navbar } from './ui'
+import { inject } from '@vercel/analytics';
+ 
+inject();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
