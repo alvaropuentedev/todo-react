59f77dd0fde2eea44136c2b468a6220b955ae029
Alvaro Puente <alvaropuentecorrea@gmail.com>
2023-08-28 20:38:59 +0200

    width full

--- src/ui/Navbar.tsx
+++ src/ui/Navbar.tsx
@@ -1,7 +1,7 @@
 
 export const Navbar = () => {
     return (
-        <nav className="bg-todo-custom-color flex justify-end items-center w-screen h-12">
+        <nav className="bg-todo-custom-color flex justify-end items-center w-full h-12">
             <ul className="mr-4 cursor-pointer">
                 <li>
                     <img src="/assets/img/account.png" alt="account" className="h-6"/>
